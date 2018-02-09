cf8710a6b126dbfdc41fbe2a802b244d52911ae9
Kim Morgan <34018948+Kirinica@users.noreply.github.com>
2018-02-09 10:43:43 -0800

    Add files via upload

---
+++ ChallengeSimpleCalculator/ChallengeSimpleCalculator/Default.aspx
@@ -0,0 +1,38 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ChallengeSimpleCalculator.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            font-family: Arial, Helvetica, sans-serif;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div style="font-family: Arial, Helvetica, sans-serif">
+            <h1>Simple Calculator</h1>
+        </div>
+        <p>
+            <span class="auto-style1">First Value: </span>&nbsp;<asp:TextBox ID="firstvalueTextBox" runat="server"></asp:TextBox>
+        </p>
+        <p class="auto-style1">
+            Second Value:&nbsp;
+            <asp:TextBox ID="secondvalueTextBox" runat="server"></asp:TextBox>
+        </p>
+        <asp:Button ID="additionButton" runat="server" Height="50px" OnClick="additionButton_Click" Text="+" Width="50px" />
+&nbsp;
+        <asp:Button ID="subtractionButton" runat="server" Height="50px" OnClick="subtractionButton_Click" Text="-" Width="50px" />
+&nbsp;
+        <asp:Button ID="multiplicationButoon" runat="server" Height="50px" OnClick="multiplicationButoon_Click" Text="*" Width="50px" />
+&nbsp;
+        <asp:Button ID="divisionButton" runat="server" Height="50px" OnClick="divisionButton_Click" Text="/" Width="50px" />
+        <br />
+        <br />
+        <asp:Label ID="resultLabel" runat="server" BackColor="Lime"></asp:Label>
+    </form>
+</body>
+</html>
---
+++ ChallengeSimpleCalculator/ChallengeSimpleCalculator/Default.aspx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeSimpleCalculator
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void additionButton_Click(object sender, EventArgs e)
+        {
+            int firstvalue = int.Parse(firstvalueTextBox.Text);
+            int secondvalue = int.Parse(secondvalueTextBox.Text);
+            int result = firstvalue + secondvalue;
+            resultLabel.Text = result.ToString();
+        }
+
+        protected void subtractionButton_Click(object sender, EventArgs e)
+        {
+            int firstvalue = int.Parse(firstvalueTextBox.Text);
+            int secondvalue = int.Parse(secondvalueTextBox.Text);
+            int result = firstvalue - secondvalue;
+            resultLabel.Text = result.ToString();
+        }
+
+        protected void multiplicationButoon_Click(object sender, EventArgs e)
+        {
+            int firstvalue = int.Parse(firstvalueTextBox.Text);
+            int secondvalue = int.Parse(secondvalueTextBox.Text);
+            int result = firstvalue * secondvalue;
+            resultLabel.Text = result.ToString();
+        }
+
+        protected void divisionButton_Click(object sender, EventArgs e)
+        {
+            int firstvalue = int.Parse(firstvalueTextBox.Text);
+            int secondvalue = int.Parse(secondvalueTextBox.Text);
+            int result = firstvalue / secondvalue;
+            resultLabel.Text = result.ToString();
+        }
+    }
+}
