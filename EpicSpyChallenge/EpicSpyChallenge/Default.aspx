dda88bb2dde380640bf1220a3deb0d794896ab9b
Kim Morgan <34018948+Kirinica@users.noreply.github.com>
2018-02-11 16:55:22 -0800

    Add files via upload

---
+++ EpicSpyChallenge/EpicSpyChallenge/Default.aspx
@@ -0,0 +1,37 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="EpicSpyChallenge.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Image ID="Image1" runat="server" ImageUrl="~/epic-spies-logo.jpg" />
+            <br />
+            <h1>Spy New Assignment Form</h1>
+            <br />
+            <br />
+            Spy Code Name:&nbsp; <asp:TextBox ID="spycodenameTextBox" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            New Assignment Name:&nbsp;
+            <asp:TextBox ID="newassignmentnameTextBox" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            End Date of Previous Assignment:<asp:Calendar ID="previousassignmentCalendar" runat="server"></asp:Calendar>
+            <br />
+            Start Date of New Assignment:<asp:Calendar ID="newassignmentCalendar" runat="server"></asp:Calendar>
+            <br />
+            Projected End Date of New Assignment:<asp:Calendar ID="enddatenewassignmentCalendar" runat="server"></asp:Calendar>
+            <br />
+            <asp:Button ID="assignspyButton" runat="server" OnClick="assignspyButton_Click" Text="Assign Spy" />
+            <br />
+            <br />
+            <asp:Label ID="resultLabel" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ EpicSpyChallenge/EpicSpyChallenge/Default.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace EpicSpyChallenge
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                previousassignmentCalendar.SelectedDate = DateTime.Now.Date;
+                newassignmentCalendar.SelectedDate = DateTime.Now.Date.AddDays(14);
+                enddatenewassignmentCalendar.SelectedDate = DateTime.Now.Date.AddDays(21);
+            }
+        }
+
+        protected void assignspyButton_Click(object sender, EventArgs e)
+        {
+            TimeSpan totalDurationOfAssignnemt = enddatenewassignmentCalendar.SelectedDate.Subtract(newassignmentCalendar.SelectedDate);
+            double totalCost = totalDurationOfAssignnemt.TotalDays * 500.00;
+
+            if (totalDurationOfAssignnemt.TotalDays > 21)
+            {
+                totalCost = +1000.0;
+            }
+
+            resultLabel.Text = string.Format("Assignment of {0} to assignment {1} is authorized. Budget total: {2:C}",
+                spycodenameTextBox.Text, newassignmentnameTextBox.Text, totalCost);
+
+            TimeSpan timeBetweenAssignments = newassignmentCalendar.SelectedDate.Subtract(previousassignmentCalendar.SelectedDate);
+            if (timeBetweenAssignments.TotalDays < 14)
+            {
+                resultLabel.Text = "Error: There must be at least two weeks between previous assignment and next assignment.";
+            }
+
+            DateTime earliestNewAssignementDate = previousassignmentCalendar.SelectedDate.AddDays(14);
+
+            newassignmentCalendar.SelectedDate = earliestNewAssignementDate;
+            newassignmentCalendar.VisibleDate = earliestNewAssignementDate;
+        }
+    }
+}
