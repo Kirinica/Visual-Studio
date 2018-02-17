437016975501b899304af31038662ce3fc95dde7
Kim Morgan <34018948+Kirinica@users.noreply.github.com>
2018-02-16 20:49:11 -0800

    Add files via upload

---
+++ ChallengeEpicSpiesAssetTracker/ChallengeEpicSpiesAssetTracker/Default.aspx
@@ -0,0 +1,43 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ChallengeEpicSpiesAssetTracker.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        <asp:Image ID="Image1" runat="server" ImageUrl="~/epic-spies-logo.jpg" />
+        <br />
+        <h1><strong>Asset Performance Tracker</strong></h1>
+        <p>
+            &nbsp;</p>
+        <p>
+            Asset Name:&nbsp;
+            <asp:TextBox ID="nameTextBox" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            Elections Rigged:&nbsp;
+            <asp:TextBox ID="riggedTextBox" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            Acts of Subterfuge Performed:&nbsp;
+            <asp:TextBox ID="actsTextBox" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            &nbsp;</p>
+        <p>
+            <asp:Button ID="addButton" runat="server" OnClick="addButton_Click" Text="Add Asset" />
+        </p>
+        <p>
+            &nbsp;</p>
+        <p>
+            <asp:Label ID="resultLabel" runat="server"></asp:Label>
+        </p>
+        <br />
+    </form>
+</body>
+</html>
---
+++ ChallengeEpicSpiesAssetTracker/ChallengeEpicSpiesAssetTracker/Default.aspx.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeEpicSpiesAssetTracker
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                string[] name = new string[0];
+                int[] rigged = new int[0];
+                int[] acts = new int[0];
+
+
+                ViewState.Add("Name", name);
+                ViewState.Add("Rigged", rigged);
+                ViewState.Add("Acts", acts);
+            }
+        }
+
+        protected void addButton_Click(object sender, EventArgs e)
+        {
+            string[] name = (string[])ViewState["Name"];
+            int[] rigged = (int[])ViewState["Rigged"];
+            int[] acts = (int[])ViewState["Acts"];
+
+            int newLength = name.Length + 1;
+
+            Array.Resize(ref name, newLength);
+            Array.Resize(ref rigged, newLength);
+            Array.Resize(ref acts, newLength);
+
+            int newIndex = name.GetUpperBound(0);
+
+            name[newIndex] = nameTextBox.Text;
+            rigged[newIndex] = int.Parse(riggedTextBox.Text);
+            acts[newIndex] = int.Parse(actsTextBox.Text);
+
+            ViewState["Name"] = name;
+            ViewState["Rigged"] = rigged;
+            ViewState["Acts"] = acts;
+
+            resultLabel.Text = string.Format("Total Elections Rigged: {0}" +
+                "<br />Average Acts of Subterfuge per Asset: {1}<br />(Last Asset you Added {2})",
+                rigged.Sum(),
+                acts.Average(),
+                name[newIndex]);
+
+            nameTextBox.Text = "";
+            riggedTextBox.Text = "";
+            actsTextBox.Text = "";
+
+        }
+    }
+}
