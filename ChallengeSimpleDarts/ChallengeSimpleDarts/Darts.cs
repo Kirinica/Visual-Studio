0a88ad0892ca863560d9b0964ab02ad285d8be3b
Kim Morgan <34018948+Kirinica@users.noreply.github.com>
2018-03-03 12:59:31 -0800

    Add files via upload

---
+++ ChallengeSimpleDarts/ChallengeSimpleDarts/Darts.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChallengeSimpleDarts
+{
+    public class Darts
+    {
+        public int Score { get; set; }
+        public bool IsDouble { get; set; }
+        public bool IsTriple { get; set; }
+
+        private Random _random;
+
+        public Darts(Random random)
+        {
+            _random = random;
+        }
+
+        public void Throw()
+        {
+            Score = _random.Next(0, 21);
+
+            int multiplier = _random.Next(1, 101);
+            if (multiplier > 95) IsTriple = true;
+            else if (multiplier > 90) IsDouble = true;
+        }
+    }
+}
